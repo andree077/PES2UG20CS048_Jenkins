709660f4e3ecb23de0658d70e93f17501bc448e2
Andre Aditya Roy <77952204+andree077@users.noreply.github.com>
2023-02-19 14:40:41 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,5 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
+  cout << "I've achieved continous integration\n";
   return 0;
 }
